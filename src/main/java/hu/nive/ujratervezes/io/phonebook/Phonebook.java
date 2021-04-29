ad86b281b54be006915f4deb008557f6f46af26a
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 11:44:27 +0200

    Implement the Phonebook class

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,19 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        try (PrintWriter writer = new PrintWriter(output)) {
+            contacts.forEach((name, phoneNumber) -> writer.printf("%s: %s%n", name, phoneNumber));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
